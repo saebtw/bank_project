130206cd085c972f46e88d207c5ace1028d46731
valerii <valeriyivanov322@gmail.com>
2024-08-11 01:50:31 -0700

    sort by date

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,9 @@ def filter_by_state(data: list) -> list:
         if item.get("state") == state:
             filtered_items.append(item)
     return filtered_items
+
+
+def sort_by_date(data: list, reverse: bool = True) -> list:
+    """Функция для сортировки словарей по дате"""
+
+    return sorted(data, key=lambda item: item.get("date"), reverse=reverse)
--- tests/processing_test.py
+++ tests/processing_test.py
@@ -33,3 +33,12 @@
 #
 # filtered_data = filter_by_state(data)
 # print(filtered_data)
+
+# def sort_by_date(data: list, reverse: bool = True) -> list:
+#     """Функция для сортировки словарей по дате"""
+#
+#
+# data = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+#
+# sorted_data = sorted(data, key=lambda item: item.get('date'), reverse=True)
+# print(sorted_data)
